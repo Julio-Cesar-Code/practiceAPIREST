6154277b4cb36600dd54f633c5db0b329b513cea
JulioCesarCode <julio111499@gmail.com>
2023-02-14 13:47:13 -0500

    add categories to homepage

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 const URLBase = 'https://api.themoviedb.org/3'
 const TrendingPreviewMoviesContainer=document.querySelector('#trendingPreview .trendingPreview-movieList')
+const categoriesContainer=document.querySelector('#categoriesPreview')
 async function getTrendingMoviesPreview() {
     const response = await fetch(`${URLBase}/trending/movie/day?api_key=${API_KEY}`);
     const data = await response.json();
     const movies = data.results;
     console.log(data, movies);
-    const moviesList = [];
+    
     movies.forEach(element => {
         //const articleContainer = document.createElement('article');
         //articleContainer.classList.add('trendingPreview-movieList')
@@ -31,4 +32,41 @@ async function getTrendingMoviesPreview() {
 
     
 }
+async function getCategoriesPreview() {
+    const response = await fetch(`${URLBase}/genre/movie/list?api_key=${API_KEY}`);
+    const data = await response.json();
+    const categories = data.genres;
+    console.log(data, categories);
+    const MaintitleCategory = document.createElement('h2');
+    MaintitleCategory.classList.add('categoriesPreview-title');
+    const titlecontent = document.createTextNode('Categorías');
+    MaintitleCategory.appendChild(titlecontent);
+    categoriesContainer.appendChild(MaintitleCategory);
+    const articleCategoryContainer = document.createElement('article');
+    articleCategoryContainer.classList.add('categoriesPreview-list')
+    categoriesContainer.appendChild(articleCategoryContainer);
+
+    categories.forEach(category => {
+        const categoryContainer = document.createElement('div');
+        
+        categoryContainer.classList.add('category-container');
+
+        const titleCategory = document.createElement('h3');
+        titleCategory.classList.add('category-title');
+        titleCategory.setAttribute('id', `id${category.id}`)
+        const titleCategoryContent = document.createTextNode(`${category.name}`);
+        titleCategory.appendChild(titleCategoryContent);
+        
+        categoryContainer.appendChild(titleCategory);
+    
+        articleCategoryContainer.appendChild(categoryContainer)
+}   )
+
+
+
+
+
+    
+}
 getTrendingMoviesPreview();
+getCategoriesPreview();
